ece7db94c2b39c872c256c2603ae9d93cf340928
emran <emranhi001@gmail.com>
2024-02-19 00:51:20 +0300

    add the front end directory which is created with npx create-react-app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login"
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
+import SignUp from './pages/SignUp';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login/>} />
+          <Route path="signup" element={<SignUp/>}/>
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
